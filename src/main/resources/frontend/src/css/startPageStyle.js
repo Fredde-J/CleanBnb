a8b08b58c8765fe29f928960b08f71c8adb1a15e
Niklas <n.hakansson@hotmail.com>
2020-03-26 21:03:06 +0100

    ReStyled the startpage

---
+++ src/main/resources/frontend/src/css/startPageStyle.js
@@ -0,0 +1,25 @@
+export const beginSearch = {
+  backgroundColor: "rgba(255, 187, 51, 0.8)", // Same as bootstraps warning.
+  border: "none",
+  borderRadius: "10px",
+  color: "rgb(117, 81, 81)",
+  cursor: "pointer"
+};
+
+export const beginSearchText = {
+  fontSize: "2em",
+  fontWeight: "normal"
+};
+
+export const buttons = {
+  color: "rgb(117, 81, 81)"
+};
+
+export const signin = {
+  border: "none"
+};
+
+export const createAccount = {
+  border: "none"
+};
+
--- src/main/resources/frontend/src/pages/StartPage.js
+++ src/main/resources/frontend/src/pages/StartPage.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+import {
+  beginSearch,
+  beginSearchText,
+  buttons,
+  signin,
+  createAccount,
+} from "../css/startPageStyle.js";
+import "../css/startPageStyle.css"
+
 const StartPage = props => {
   const goToSearch = () => {
     props.history.push("/search");
@@ -7,47 +16,64 @@ const StartPage = props => {
 
   return (
     <div className="container">
-      <div className="begin-search card m-4">
-        <div className="card-body" onClick={goToSearch}>
-          <h3 className="text-center">Börja leta bostad!</h3>
+      <div
+        className="begin-search card row col-12 col-md-6 mx-auto mt-5 mb-5"
+        style={beginSearch}
+        onClick={goToSearch}
+      >
+        <div className="begin-search-wrapper p-3">
+          <h3 className="begin-search-text text-center" style={beginSearchText}>
+            Börja leta bostad!
+          </h3>
         </div>
       </div>
 
-      <div className="sign-in-form card m-4 bg-transparent">
-        <div className="card-body">
-        <p className="text-white text-center mb-2">Har du ett konto?</p>
-          <form className="row">
-            <div className="form-group col-12">
+      <div className="signin-form card m-4 bg-transparent" style={signin}>
+        <p className="text-white text-center m-0 font-weight-bold">
+          Har du ett konto?
+        </p>
+        <form className="p-2">
+          <div className="row">
+            <div className="form-group col-12 col-md-7 mx-auto">
               <label className="text-white font-weight-bold" htmlFor="user">
                 Användarnamn
               </label>
-              <input
-                type="email"
-                className="form-control"
-                id="user"
-                aria-describedby="emailHelp"
-              />
+              <input type="email" className="form-control" id="user" />
             </div>
-            <div className="form-group col-12">
+            <div className="form-group col-12 col-md-7 mx-auto">
               <label className="text-white font-weight-bold" htmlFor="password">
                 Lösenord
               </label>
               <input type="password" className="form-control" id="password" />
             </div>
-            <div className="form-group form-check"></div>
-            <button type="submit" className="btn btn-warning mt-2 col-6">
+          </div>
+
+          <div className="row">
+            <button
+              type="submit"
+              className="btn btn-warning mt-2 col-8 col-md-3 mx-auto"
+              style={buttons}
+            >
               Logga in
             </button>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
 
-      <div className="create-account row">
-        <p className="text-center text-white col-12">Vill du skapa ett konto?</p>
-        <button className="btn btn-warning mt-2 col-4 offset-3 ">Skapa Konto</button>col-6
+      <div
+        className="create-account card m-4 bg-transparent row"
+        syle={createAccount}
+      >
+        <p className="text-center text-white col-12 m-0 font-weight-bold">
+          Inte registrerad?
+        </p>
+        <button
+          className="btn btn-warning mt-2 col-8 col-md-3 mx-auto"
+          style={buttons}
+        >
+          Skapa Konto
+        </button>
       </div>
-
-      
     </div>
   );
 };
