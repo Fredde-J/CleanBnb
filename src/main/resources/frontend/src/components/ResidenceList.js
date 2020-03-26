0c09c80e5ba75d6c18e14453d3a4eb8368e0649d
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-03-26 14:09:09 +0100

    removed img
    fredde o martin

--- src/main/resources/frontend/src/components/ResidenceList.js
+++ src/main/resources/frontend/src/components/ResidenceList.js
@@ -8,7 +8,6 @@ const ResidenceList = ({ result }) => {
   return residences.map((residence, i) => {
     return(
     <div style={{ borderRadius: "15px"}} className="Card bg-warning my-3">
-      {/* <img src={result.image} alt="" className="card-img-top p-3" /> */}
        <div className="card-body row px-2">
           <h5 className="card-title col-6 text-left">
             {residence.address}
