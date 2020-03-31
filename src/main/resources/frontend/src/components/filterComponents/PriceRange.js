2896901d155e56c2ed02ef3f4c110febb132dec2
Yusra <yossan@hotmail.se>
2020-03-31 13:50:32 +0200

    Shit fix
    Niklas, Yusra

--- src/main/resources/frontend/src/components/filterComponents/PriceRange.js
+++ src/main/resources/frontend/src/components/filterComponents/PriceRange.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Row } from "reactstrap";
 
 const PriceRange = () => {
   const [price, setPrice] = useState(0);
@@ -9,10 +10,10 @@ const PriceRange = () => {
 
   return (
     <div className="price-range-wrapper mb-4">
-      <div className="price-range row">
+      <div className="price-range">
         <p className="col-12 text-center m-0">Pris per Natt</p>
         <p className="col-12 text-center m-0 mb-2">{price}</p>
-        <div className="row col-12">
+        <Row>
           <p className="col-3 text-right m-0 mb-2">min</p>
           <input
             type="range"
@@ -24,7 +25,7 @@ const PriceRange = () => {
             value={price}
           />
           <p className="col-3 m-0">max</p>
-        </div>
+        </Row>
       </div>
     </div>
   );
