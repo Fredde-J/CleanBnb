18b8624fce92876178c7328202fc4dc3bc7b72ec
Martin Hellström <martver12@gmail.com>
2020-04-01 09:27:42 +0200

    fixed logo to the best

--- src/main/resources/frontend/src/components/Header.js
+++ src/main/resources/frontend/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
     <>
       <Navbar color="warning" light expand="md">
         <Link to="/" className="center header-navbarTitle mr-3">
-          ClearBnB
+          <img src="/images/logo.png"/>
         </Link>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
