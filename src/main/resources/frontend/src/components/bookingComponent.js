e89c4f5ef5d8067ce2be43315e5a79a1ffaed2de
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-04-09 12:38:53 +0200

    Fixed add booking to working
    Fredde, Niklas & Martin

--- src/main/resources/frontend/src/components/bookingComponent.js
+++ src/main/resources/frontend/src/components/bookingComponent.js
@@ -1,65 +1,49 @@
-import React, { useContext,useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ResidenceContext } from "../contexts/ResidenceContext";
 import { Form, FormGroup, Label, Input, Col, Row, Button } from "reactstrap";
 import { divStyle1, imgStyle, topPStyle } from "../css/bookingComponentStyle";
 import { UserContext } from "../contexts/UserContext";
-import {BookingContext} from "../contexts/BookingContext";
 
 const BookingComponent = (props) => {
   const { chosenResidence } = useContext(ResidenceContext);
   const { user } = useContext(UserContext);
-  const { setBooking } = useContext(BookingContext);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [residenceId, setResidenceId] = useState("");
-  const [userId, setUserId] = useState("");
-  const bookingInfo = {
-    startDate,
-    endDate,
-    residenceId,
-    userId,
+  const [bookingInfo, setBookingInfo] = useState({
+    startDate: null,
+    endDate: null,
+    residenceId: null,
+    userId: null
+  });
+
+  const createBooking = () => {
+    setBookingInfo({
+      startDate: "2020-05-01",
+      endDate: "2020-05-010",
+      residenceId: chosenResidence.residenceId,
+      userId: user.userId,
+    });
+
   };
+  useEffect(() => {
+    if(bookingInfo.endDate){
+      fetchBookings(bookingInfo)
+    }
+  }, [bookingInfo]);
 
- 
-
-  if (!chosenResidence) {
-    console.log("not here");
-  }
-  console.log(chosenResidence);
-
-  const goToBookingConfirmation =async(e)=>{
-    e.preventDefault();
-    
-     setStartDate("2020-05-05")
-     setEndDate("2020-05-10")
-     setResidenceId(chosenResidence.residenceId)
-     setUserId(user.userId)
-    fetchBookings(bookingInfo);
-  
-  }
-   useEffect(() => {
-     console.log(bookingInfo);
-   }, [bookingInfo]);
-
- 
-
-  const fetchBookings = async bookingInfo =>{
+  const fetchBookings = async (bookingInfo) => {
     let response = await fetch("/rest/bookings", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(bookingInfo),
     });
-      try {
-            response = await response.json();
-            setBooking(response);
-           // props.history.push(
-           //   `/account/${chosenResidence.residenceId}/bookingConfirmation`
-            //);
-          } catch {
-        console.log("Fel inmatning av uppgifter");
-      }
-  }
-  
+    try {
+      response = await response.json();
+      props.history.push(
+        `/account/${chosenResidence.residenceId}/bookingConfirmation`
+      );
+    } catch {
+      console.log("Fel inmatning av uppgifter");
+    }
+  };
 
   return (
     <Row>
@@ -85,7 +69,7 @@ const BookingComponent = (props) => {
                         type="text"
                         name="name"
                         id="firstName"
-                        value={user.firstName}
+                        defaultValue={user.firstName}
                       />
                     ) : (
                       <Input type="text" name="name" id="firstName" />
@@ -100,7 +84,7 @@ const BookingComponent = (props) => {
                         type="lastname"
                         name="lastname"
                         id="lastName"
-                        value={user.lastName}
+                        defaultValue={user.lastName}
                       />
                     ) : (
                       <Input type="lastname" name="lastname" id="lastName" />
@@ -118,7 +102,7 @@ const BookingComponent = (props) => {
                         type="email"
                         name="email"
                         id="emailAdress"
-                        value={user.username}
+                        defaultValue={user.username}
                       />
                     ) : (
                       <Input type="email" name="email" id="emailAdress" />
@@ -133,7 +117,6 @@ const BookingComponent = (props) => {
                       name="number"
                       id="phoneNumber"
                       display="inline-block"
-                      
                     />
                   </FormGroup>
                 </Col>
@@ -143,7 +126,7 @@ const BookingComponent = (props) => {
                 color="secondary"
                 block
                 className="col-12 col-md-8 offset-md-2"
-                onClick={goToBookingConfirmation}
+                onClick={createBooking}
               >
                 Fortsätt
               </Button>
